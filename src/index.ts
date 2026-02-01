235711f09b5dd6f3f2f15aa5fc746ed0a9aff540
RACunderskrt <manuel.humbert@gmail.com>
2026-02-01 21:51:12 +0100

    rabbitmq added

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import amqp from 'amqplib';
 import { LogsRepo } from './adapters/driven/logsRepo';
 import { LogsService } from './services/logsService';
 import { LogsController } from './adapters/driving/logsController';
-
 
 dotenv.config();
 
@@ -26,3 +26,29 @@ logs_controller.registerRoutes(app);
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
+
+export async function startLogsConsumer(service_logs: any) {
+  	const connection = await amqp.connect('amqp://microservice:microservice@localhost:5672');
+
+	const channel = await connection.createChannel();
+	const queue = 'logs_queue';
+
+	await channel.assertQueue(queue, { durable: true });
+
+	channel.consume(queue, async (msg: any) => {
+		if (!msg) return;
+
+		try {
+			const payload = JSON.parse(msg.content.toString());
+			await service_logs.createLogs(payload);
+			console.log("J'AI RECU LE MSG", payload);
+			channel.ack(msg);
+		} catch (err) {
+			console.error('Failed to process log message', err);
+			channel.nack(msg, false, false); // discard
+		}
+	});
+}
+
+startLogsConsumer(logs_service);
+
